78484fa6ba761c8a9e80073b061f09df2a5f04ce
GuWon-Kang <guwon91@gmail.com>
2024-03-28 01:09:08 +0900

    24.03.28 Am 1:09

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let string = String::from("127.0.0.1:8080");
+    let string_slice = &string[10..];
+
+    dbg!(string);
+    dbg!(string_slice);
+
+    // let server = Server::new("127.0.0.1:8080");
+    // server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr
+        }
+    }
+    fn run(self) {
+
+    }
 }
