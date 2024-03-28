63abd3fddcc12c7edb907687f790d74be5ba6884
FreeHunter <chronos@kakao.com>
2024-03-28 22:37:46 +0900

    study 24.03.28 10:37 pm

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
+use http::Method;
+use http::Request;
+use server::Server;
+
+mod http;
+mod server;
+
 fn main() {
-    let string = String::from("127.0.0.1:8080");
-    let string_slice = &string[10..];
-
-    dbg!(string);
-    dbg!(string_slice);
-
-    // let server = Server::new("127.0.0.1:8080");
-    // server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr
-        }
-    }
-    fn run(self) {
-
-    }
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
 }
